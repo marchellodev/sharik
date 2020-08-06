de15f6825c7683bdd11fb7da13d98a9fbaff2575
mrfoxie <siddh.mistry396321@gmail.com>
2020-08-06 17:35:24 +0530

    india-Hindi language added

---
+++ lib/locales/in-gu.dart
@@ -0,0 +1,49 @@
+Map<String, String> get getIN_gu => {
+      'CONNECT': 'जुडिये',
+      'Connect devices to the same network - use Wi-Fi or Mobile Hotspot':
+          'डिवाइस को एक ही नेटवर्क से कनेक्ट करें - वाई-फाई या मोबाइल हॉटस्पॉट का उपयोग करें',
+      'SEND': 'भेजें',
+      'Select any file you like': 'अपनी पसंद की कोई भी फाइल चुनें',
+      'RECEIVE': 'प्राप्त करना',
+      'Open given link on another device in any browser':
+          'किसी भी ब्राउज़र में किसी अन्य डिवाइस पर दिया गया लिंक खोलें',
+      'DONE': 'किया हुआ',
+      'NEXT': 'आगे',
+      'loading...': 'लोड हो रहा है...',
+      'Mobile Hotspot': 'मोबाइल हॉटस्पॉट',
+      'Connect to': 'जुड़े',
+      'or set up a': 'या सेट अप ए',
+      ' Mobile Hotspot': ' मोबाइल हॉटस्पॉट',
+      'Not connected': 'जुड़े नहीं हैं',
+      'Now open this link': 'अब इस लिंक को ओपन करें',
+      'in any browser': 'किसी भी ब्राउज़र में',
+      'The recipient needs to be connected': 'प्राप्तकर्ता को कनेक्ट करने की आवश्यकता है',
+      'to the same network': 'एक ही नेटवर्क के लिए',
+      'Select file': 'फ़ाइल का चयन करें',
+      'Latest': 'नवीनतम',
+      'Undefined': 'अपरिभाषित',
+      'EVERYWHERE': 'हर जगह',
+      'Sharik is available for Android, Windows, MacOS and Linux!':
+          'एंड्रॉइड, विंडोज, मैकओएस और लिनक्स के लिए शारिक उपलब्ध है!',
+      'Click here to learn more': 'और जानने के लिए यहां क्लिक करे',
+      'Copied to Clipboard': 'क्लिपबोर्ड पर नकल',
+      'Type some text': 'कुछ टेक्स्ट टाइप करें',
+      'Close': 'बंद करे',
+      'Send': 'संदेश',
+      'Text': 'टेक्स्ट',
+      'App': 'ऐप',
+      'Hide system apps': 'सिस्टम ऐप्स छिपाएं',
+      'Hide non-launchable apps': 'गैर-प्रशंसनीय एप्लिकेशन छिपाएँ',
+      'Search': 'खोज',
+      //v2.1
+      'Updates': 'अपडेट',
+      'The latest version is already installed':
+          'नवीनतम संस्करण पहले से ही स्थापित है',
+      'Current version': 'वर्तमान संस्करण',
+      'The latest version': 'नवीनतम संस्करण',
+      'Changelog': 'बदलाव का',
+
+      //v2.2
+      'Receiver': 'रिसीवर'
+      //https://siddhmistry.surge.sh
+    };
