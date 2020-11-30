71c40b7976a9bd20257d2c54f4a6e4b37e572d6b
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-11-30 16:51:11 +0300

    ➕ Add Arabic translation

---
+++ lib/locales/ar.dart
@@ -0,0 +1,54 @@
+Map<String, String> get getAr => {
+      'CONNECT': 'اتصل',
+      'Connect devices to the same network - use Wi-Fi or Mobile Hotspot':
+          'اوصل الأجهزة الى نفس الشبكة - استخدم الواي فاي أو نقطة اتصال',
+      'SEND': 'إرسال',
+      'Select any file you like': 'اختر أي ملف تريد',
+      'RECEIVE': 'استلم',
+      'Open given link on another device in any browser':
+          'افتح الرابط على جهاز آخر أو في أي متصفح',
+      'DONE': 'إنهاء',
+      'NEXT': 'التالي',
+      'loading...': 'جاري التحميل...',
+      'Mobile Hotspot': 'نقطة اتصال',
+      'Connect to': 'اتصل بـ',
+      'or set up a': 'أو قم بإعداد',
+      ' Mobile Hotspot': ' نقطة اتصال',
+      'Not connected': 'غير متصل',
+      'Now open this link': 'الآن افتح هذا الرابط',
+      'in any browser': 'في أي متصفح',
+      'The recipient needs to be connected': 'يجب أن يكون المستلم متصلاً',
+      'to the same network': 'بنفس الشبكة',
+      'Select file': 'اختر ملف',
+      'Latest': 'الأحدث',
+      'Undefined': 'غير معروف',
+      'EVERYWHERE': 'في كل مكان',
+      'Sharik is available for Android, Windows, MacOS and Linux!':
+          'تطبيق شارك متوفر للأندرويد، والويندوز، والماك، واللينكس!',
+      'Click here to learn more': 'اضغط هنا لمعرفة المزيد',
+      'Copied to Clipboard': 'نسخ إلى الحافظة',
+      'Type some text': 'أكتب شيئاً',
+      'Close': 'إغلاق',
+      'Send': 'إرسال',
+      'Text': 'نص',
+      'App': 'تطبيق',
+      'Hide system apps': 'إخفاء تطبيقات النظام',
+      'Hide non-launchable apps': 'إخفاء التطبيقات التي لا يمكن تشغيلها',
+      'Search': 'البحث',
+      //v2.1
+      'Updates': 'التحديثات',
+      'The latest version is already installed': 'النسخة الأحدث مثبتة بالفعل',
+      'Current version': 'النسخة الحالية',
+      'The latest version': 'النسخة الأحدث',
+      'Changelog': 'سجل التغييرات',
+
+      //v2.2
+      'Receiver': 'المستلم',
+
+      //v2.4 (fonts)
+      'Andika': 'Andika',
+      'Comfortaa': 'Comfortaa',
+
+      //v2.5 (ios)
+      'Gallery': 'استديو الصور'
+    };
