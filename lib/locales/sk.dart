70a1f77d5e85d26f173868b0f6ae82ed3352dd62
Tibor Repček <tiborepcek@gmail.com>
2020-12-30 17:26:37 +0100

    Create sk.dart

---
+++ lib/locales/sk.dart
@@ -0,0 +1,54 @@
+Map<String, String> get getSk => {
+      'CONNECT': 'PRIPOJIŤ',
+      'Connect devices to the same network - use Wi-Fi or Mobile Hotspot':
+          'Pripojte zariadenia do rovnakej siete - použite Wi-Fi alebo mobilný prístupový bod',
+      'SEND': 'POSLAŤ',
+      'Select any file you like': 'Vyberte súbor',
+      'RECEIVE': 'PRIJAŤ',
+      'Open given link on another device in any browser':
+          'Otvorte tento odkaz na inom zariadení v ľubovoľnom webovom prehliadači',
+      'DONE': 'HOTOVO',
+      'NEXT': 'ĎALEJ',
+      'loading...': 'načítava sa...',
+      'Mobile Hotspot': 'Mobilný prístupový bod',
+      'Connect to': 'Pripojiť k',
+      'or set up a': 'alebo vytvorte',
+      ' Mobile Hotspot': ' mobilný prístupový bod',
+      'Not connected': 'Nepripojený',
+      'Now open this link': 'Teraz otvorte tento odkaz',
+      'in any browser': 'v ľubovoľnom webovom prehliadači',
+      'The recipient needs to be connected': 'Príjemca musí byť pripojený',
+      'to the same network': 'k rovnakej sieti',
+      'Select file': 'Vyberte súbor',
+      'Latest': 'Najnovšie',
+      'Undefined': 'Neurčený',
+      'EVERYWHERE': 'KDEKOĽVEK',
+      'Sharik is available for Android, Windows, MacOS and Linux!':
+          'Sharik je k dispozícii pre Android, Windows, MacOS a Linux!',
+      'Click here to learn more': 'Kliknite sem a dozviete sa viac',
+      'Copied to Clipboard': 'Skopírované do schránky',
+      'Type some text': 'Napíšte text',
+      'Close': 'Zavrieť',
+      'Send': 'Poslať',
+      'Text': 'Text',
+      'App': 'Aplikácia',
+      'Hide system apps': 'Skryť systémové aplikácie',
+      'Hide non-launchable apps': 'Skryť nespustiteľné aplikácie',
+      'Search': 'Hľadať',
+      //v2.1
+      'Updates': 'Aktualizácie',
+      'The latest version is already installed': 'Máte nainštalovanú najnovšiu verziu',
+      'Current version': 'Aktuálna verzia',
+      'The latest version': 'Najnovšia verzia',
+      'Changelog': 'Zoznam zmien',
+
+      //v2.2
+      'Receiver': 'Prijímateľ',
+
+      //v2.4 (fonts)
+      'Andika': 'Andika',
+      'Comfortaa': 'Comfortaa',
+
+      //v2.5 (ios)
+      'Gallery': 'Galéria'
+    };
