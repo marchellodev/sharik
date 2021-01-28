ffabf7d882794b0eaff55109fe277c34d23db83a
Behzad Najafzadeh <behzad.njf22@gmail.com>
2021-01-28 15:04:18 +0330

    Add files via upload

---
+++ lib/locales/fa.dart
@@ -0,0 +1,54 @@
+Map<String, String> get getFa => {
+      'CONNECT': 'اتصال',
+      'Connect devices to the same network - use Wi-Fi or Mobile Hotspot':
+          'هر دو دستگاه را به یک شبکه وصل کنید - میتوانید از Wi-Fi یا Hotspot موبایل استفاده کنید',
+      'SEND': 'ارسال',
+      'Select any file you like': 'فایل مورد نظر را انتخاب کنید',
+      'RECEIVE': 'دریافت',
+      'Open given link on another device in any browser':
+          'لینک تولید شده را در دستگاه دیگر در هر مرورگری وارد کنید',
+      'DONE': 'تمام',
+      'NEXT': 'بعدی',
+      'loading...': 'لطفا صبر کنید...',
+      'Mobile Hotspot': 'نقطه اتصال',
+      'Connect to': 'اتصال به ',
+      'or set up a': 'یا راه اندازی یک ',
+      ' Mobile Hotspot': ' نقطه اتصال',
+      'Not connected': 'اتصال برقرار نیست',
+      'Now open this link': 'میتوانید لینک زیر را ',
+      'in any browser': 'در هر مرورگری باز کنید',
+      'The recipient needs to be connected': 'دریافت کننده حتما باید ',
+      'to the same network': 'به همان شبکه متصل باشد',
+      'Select file': 'انتخاب فایل',
+      'Latest': 'آخرین فایل ها',
+      'Undefined': 'تعریف نشده',
+      'EVERYWHERE': 'در همه جا',
+      'Sharik is available for Android, Windows, MacOS and Linux!':
+          'شریک برای اندروید، ویندوز ، MacOS و لینوکس موجود است!',
+      'Click here to learn more': 'برای توضیحات بیشتر اینجا کلیک کنید',
+      'Copied to Clipboard': 'در حافظه کپی شد',
+      'Type some text': 'متن را وارد کنید',
+      'Close': 'خروج',
+      'Send': 'ارسال',
+      'Text': 'متن',
+      'App': 'برنامه',
+      'Hide system apps': 'مخفی کردن برنامه های سیستمی',
+      'Hide non-launchable apps': 'مخفی کردن برنامه های  غیراجرایی',
+      'Search': 'جستجو',
+      //v2.1
+      'Updates': 'بروزرسانی ها',
+      'The latest version is already installed': 'آخرین نسخه نصب شده است',
+      'Current version': 'نسخه کنونی',
+      'The latest version': 'آخرین نسخه',
+      'Changelog': 'تغییرات',
+
+      //v2.2
+      'Receiver': 'دریافت کننده',
+
+      //v2.4 (fonts)
+      'Andika': 'Tajawal',
+      'Comfortaa': 'Changa',
+
+      //v2.5 (ios)
+      'Gallery': 'گالری'
+    };
