091129fe5a8fb4c42c1706d5250a0833ad157fc5
marchellodev <marchellodev@gmail.com>
2021-06-30 13:01:28 +0300

    windows msi packaging small tweaks

--- windows/packaging/msi/build.sh
+++ windows/packaging/msi/build.sh
@@ -1,3 +1,3 @@
 # https://github.com/Ed-ITSolutions/create-msi-installer-from-folder
 
-.\create-msi-installer-from-folder.ps1 -Path "C:\Users\markm\Downloads\create-msi-installer-from-folder-0.0.1\app" -Product "Sharik" -Version "3.0.0" -executable "sharik.exe"
+.\create-msi-installer-from-folder.ps1 -Path "C:\Users\markm\Desktop\sharik\windows\packaging\msi\app" -Product "Sharik" -Version "3.0.0" -executable "sharik.exe" -Manufacturer "marchellodev" -AboutLink "https://github.com/marchellodev/sharik"
