8162b04ccd12695e5573d00a3de5815f21d3d5f9
marchellodev <marchellodev@gmail.com>
2021-08-05 16:24:01 +0300

    Download all button now words properly & bits of refactoring

--- lib/logic/services/sharing_service.dart
+++ lib/logic/services/sharing_service.dart
@@ -75,6 +75,7 @@ class SharingService extends ChangeNotifier {
     }
 
     await for (final request in _server!) {
+      // If we are requesting sharik.json
       if (request.requestedUri.toString().split('/').length == 4 &&
           request.requestedUri.toString().split('/').last == 'sharik.json') {
         request.response.headers.contentType =
@@ -86,139 +87,128 @@ class SharingService extends ChangeNotifier {
           'os': Platform.operatingSystem,
         }));
         request.response.close();
-      } else {
-        if (_file.type == SharingObjectType.file ||
-            _file.type == SharingObjectType.app) {
-          if (!_file.data.contains(multipleFilesDelimiter)) {
-            final f = File(_file.data);
-            // todo what if the file is a folder?
-            // todo check sharing multiple apps & media files
-            final size = await f.length();
+        continue;
+      }
 
-            request.response.headers.contentType =
-                ContentType('application', 'octet-stream', charset: 'utf-8');
+      // If we are sharing text
+      if (_file.type == SharingObjectType.text) {
+        request.response.headers.contentType =
+            ContentType('text', 'plain', charset: 'utf-8');
+        request.response.write(_file.data);
+        request.response.close();
+        continue;
+      }
 
-            request.response.headers.add(
-              'Content-Transfer-Encoding',
-              'Binary',
-            );
+      // If we are sharing only one file that is not a folder
+      if (!_file.data.contains(multipleFilesDelimiter) &&
+          FileSystemEntity.typeSync(_file.data) !=
+              FileSystemEntityType.directory) {
+        final f = File(_file.data);
+        final size = await f.length();
 
-            request.response.headers.add(
-              'Content-disposition',
-              'attachment; filename="${Uri.encodeComponent(_file.type == SharingObjectType.file ? _file.name : '${_file.name}.apk')}"',
-            );
-            request.response.headers.add(
-              'Content-length',
-              size,
-            );
+        _pipeFile(
+            request,
+            f,
+            size,
+            _file.type == SharingObjectType.file
+                ? _file.name
+                : '${_file.name}.apk');
+        continue;
+      }
 
-            await f
-                .openRead()
-                .pipe(request.response)
-                .catchError((e) {})
-                .then((a) {
-              request.response.close();
-            });
-          } else {
-            final fileList = _file.data.split(multipleFilesDelimiter);
+      // All other cases
 
-            final requestedFilePath =
-                request.requestedUri.queryParameters['q'] ?? '';
-            File? file;
-            int? size;
-            var isDir = false;
+      final fileList = _file.data.split(multipleFilesDelimiter);
 
-            // if the file is requested
-            if (requestedFilePath.isNotEmpty) {
-              isDir = await FileSystemEntity.type(requestedFilePath) ==
-                  FileSystemEntityType.directory;
-              // todo is that secure enough?
-              if (!fileList.contains(requestedFilePath)) {
-                // checking if the path belongs to a shared folder
-                var isInsideAFolder = false;
-                for (final el in fileList) {
-                  if (requestedFilePath.contains(el)) {
-                    isInsideAFolder = true;
-                  }
-                }
+      final requestedFilePath = request.requestedUri.queryParameters['q'] ?? '';
+      File? file;
+      int? size;
+      var isDir = false;
 
-                if (!isInsideAFolder) {
-                  print('NO ACCESS!!!');
-                  continue;
-                }
-              }
-
-              if (!isDir) {
-                file = File(requestedFilePath);
-                size = await file.length();
-              }
-            }
-
-            // We are sharing multiple files
-            // Serving an entry html page or the folder page
-            if (requestedFilePath.isEmpty || isDir) {
-              final _fileList = isDir
-                  ? Directory(requestedFilePath)
-                      .listSync()
-                      .map((e) => e.path)
-                      .toList()
-                  : fileList;
-
-              final displayFiles = Map.fromEntries(_fileList.map((e) =>
-                  MapEntry(
-                      e,
-                      FileSystemEntity.typeSync(e) !=
-                          FileSystemEntityType.directory)));
-
-              request.response.headers.contentType =
-                  ContentType('text', 'html', charset: 'utf-8');
-              request.response.write(_buildHTML(displayFiles));
-              request.response.close();
-              // Serving the files
-            } else {
-              request.response.headers.contentType =
-                  ContentType('application', 'octet-stream', charset: 'utf-8');
-
-              request.response.headers.add(
-                'Content-Transfer-Encoding',
-                'Binary',
-              );
-
-              request.response.headers.add(
-                'Content-disposition',
-                'attachment; filename="${Uri.encodeComponent(_file.type == SharingObjectType.file ? requestedFilePath.split(Platform.pathSeparator).last : '${_file.name}.apk')}"',
-              );
-
-              if (size != null) {
-                request.response.headers.add(
-                  'Content-length',
-                  size,
-                );
-              }
-
-              await file!
-                  .openRead()
-                  .pipe(request.response)
-                  .catchError((e) {})
-                  .then((a) {
-                request.response.close();
-                // serving all the files or an entry html page
-              });
+      // if the file is requested
+      if (requestedFilePath.isNotEmpty) {
+        isDir = await FileSystemEntity.type(requestedFilePath) ==
+            FileSystemEntityType.directory;
+        // todo is that secure enough?
+        if (!fileList.contains(requestedFilePath)) {
+          // checking if the path belongs to a shared folder
+          var isInsideAFolder = false;
+          for (final el in fileList) {
+            if (requestedFilePath.contains(el)) {
+              isInsideAFolder = true;
             }
           }
-        } else {
-          request.response.headers.contentType =
-              ContentType('text', 'plain', charset: 'utf-8');
-          request.response.write(_file.data);
-          request.response.close();
+
+          if (!isInsideAFolder) {
+            print('NO ACCESS!!!');
+            continue;
+          }
         }
+
+        if (!isDir) {
+          file = File(requestedFilePath);
+          size = await file.length();
+        }
+      }
+
+      // We are sharing multiple files
+      // Serving an entry html page or the folder page
+      if (requestedFilePath.isEmpty || isDir) {
+        final _fileList = isDir
+            ? Directory(requestedFilePath)
+                .listSync()
+                .map((e) => e.path)
+                .toList()
+            : fileList;
+
+        final displayFiles = Map.fromEntries(_fileList.map((e) => MapEntry(e,
+            FileSystemEntity.typeSync(e) != FileSystemEntityType.directory)));
+
+        request.response.headers.contentType =
+            ContentType('text', 'html', charset: 'utf-8');
+        request.response.write(_buildHTML(displayFiles));
+        request.response.close();
+        // Serving the files
+      } else {
+        _pipeFile(
+            request,
+            file,
+            size,
+            _file.type == SharingObjectType.file
+                ? requestedFilePath.split(Platform.pathSeparator).last
+                : '${_file.name}.apk');
       }
     }
   }
 }
 
-// todo fix Download all
-// todo pass type (file vs folder)
+Future<void> _pipeFile(
+    HttpRequest request, File? file, int? size, String fileName) async {
+  request.response.headers.contentType =
+      ContentType('application', 'octet-stream', charset: 'utf-8');
+
+  request.response.headers.add(
+    'Content-Transfer-Encoding',
+    'Binary',
+  );
+
+  request.response.headers.add(
+    'Content-disposition',
+    'attachment; filename="${Uri.encodeComponent(fileName)}"',
+  );
+
+  if (size != null) {
+    request.response.headers.add(
+      'Content-length',
+      size,
+    );
+  }
+
+  await file!.openRead().pipe(request.response).catchError((e) {}).then((a) {
+    request.response.close();
+  });
+}
+
 /// bool - true if the path is a file; false if it's a folder
 String _buildHTML(Map<String, bool> files) {
   final html = '''
@@ -231,23 +221,35 @@ String _buildHTML(Map<String, bool> files) {
   <body>
     <button onClick="downloadAll()">Download all (Will skip folders, Requires JS enabled)</button>
     <ul style="line-height:200%">
-      ${files.entries.map((val) => '<li><a href="/?q=${Uri.decodeComponent(val.key)}" class="${val.value ? 'file' : 'folder'}"><b>${val.key.split(Platform.pathSeparator).last}</b> (${val.key})</li></a>').join('\n')}
+      ${files.entries.map((val) => '<li><a href="/?q=${Uri.decodeComponent(val.key)}" class="${val.value ? 'file' : 'folder'}"><b>${val.key.split(Platform.pathSeparator).last}</b> <small>(${val.key})</small></li></a>').join('\n')}
     </ul>
     
     <script>
+    // Adapted from https://web.archive.org/web/20210805125534/https://developpaper.com/question/how-to-download-multiple-url-files-with-js/ 
+    
+    let triggerDelay = 100;
+    let removeDelay = 1000; 
+    
     function downloadAll(){
       var arr = [].slice.call(document.getElementsByClassName('file'));
-      downloadList(arr);
+      arr.forEach(function(item,index){
+        _createIFrame(item.href, index * triggerDelay, removeDelay);
+      });
     }
-
-    function downloadList(list){
-      list[0].click();
-      list.shift();
     
+    function _createIFrame(url, triggerDelay, removeDelay) {
       setTimeout(function() {
-        if(list.length > 0) downloadList(list);
-      }, 1000);
-    }
+        var node = document.createElement("iframe");
+        node.setAttribute("style", "display: none;");
+        node.setAttribute("src", url);
+        document.body.appendChild(node);
+    
+        setTimeout(function() {
+            node.remove();
+        }, removeDelay);
+        
+      }, triggerDelay);
+    } 
     </script>
   </body>
 </html>
