dfea1351686263248aafa48ee2280fdea84f5b90
marchellodev <marchellodev@gmail.com>
2022-08-31 11:45:50 +0300

    Create a translations sync script [re-written in Dart]

---
+++ scripts/crowdin.dart
@@ -0,0 +1,126 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:archive/archive.dart';
+import 'package:http/http.dart' as http;
+
+const projectId = 458738;
+const apiUrl = 'https://api.crowdin.com/api/v2/projects/$projectId';
+
+String getToken() => Platform.environment['TOKEN']!;
+
+// todo
+// - change list of languages in the app
+// - translations for stores & f-droid
+
+void main() async {
+  await clearCurrentTranslations();
+  final id = await buildProjectTranslations();
+  // const id = 40;
+  await applyProjectTranslations(id);
+}
+
+Future<int> buildProjectTranslations() async {
+  final buildReq = await http.post(
+    Uri.parse('$apiUrl/translations/builds'),
+    body: jsonEncode({}),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ${getToken()}',
+    },
+  );
+
+  final buildId = jsonDecode(buildReq.body)['data']['id'] as int;
+  print('building project translations (id $buildId)');
+
+  var built = false;
+
+  while (!built) {
+    await Future.delayed(const Duration(seconds: 1));
+    final buildStatus = await http.get(
+      Uri.parse('$apiUrl/translations/builds/$buildId'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${getToken()}',
+      },
+    );
+
+    final status = jsonDecode(buildStatus.body)['data']['status'] as String;
+
+    if (status == 'finished') {
+      built = true;
+    }
+  }
+
+  return buildId;
+}
+
+// todo it would be cool to display diff [for changelog] after applying translations
+Future<void> applyProjectTranslations(int buildId) async {
+  print('downloading project translations (id $buildId)');
+
+  final buildDownload = await http.get(
+    Uri.parse('$apiUrl/translations/builds/$buildId/download'),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ${getToken()}',
+    },
+  );
+
+  final buildFile = jsonDecode(buildDownload.body)['data']['url'] as String;
+
+  final buildFileRaw = (await http.get(Uri.parse(buildFile))).bodyBytes;
+
+  final inputStream = InputStream(buildFileRaw);
+  final archive = ZipDecoder().decodeBuffer(inputStream);
+
+  for (final file in archive.files) {
+    if (!file.isFile) {
+      continue;
+    }
+
+    if (!file.name.contains('arb')) {
+      // todo handle store.csv & contributors.csv
+      continue;
+    }
+
+    // fr/fr_FR.arb
+    var fileName = file.name.split('/')[0];
+    fileName = fileName.replaceAll('-', '_');
+
+    var contents = utf8.decode(file.content as List<int>);
+    contents = contents.replaceAll(
+      '"@@locale": "en",',
+      '',
+      // '"@@locale": "$locale",',
+    );
+
+    // fa_IR.arb
+    // Exception: Multiple arb files with the same 'id' locale detected.
+    // id_ID.arb
+    File('locales/app_$fileName.arb').writeAsStringSync(contents);
+
+    if (fileName.contains('_')) {
+      final newFileName = fileName.split('_')[0];
+      File('locales/app_$newFileName.arb').writeAsStringSync('{}');
+    }
+  }
+
+  // not deleting this empty file causes a build error
+  File('locales/app_tlh.arb').deleteSync();
+}
+
+Future<void> clearCurrentTranslations() async {
+  final dir = Directory('locales');
+  final files = await dir.list().toList();
+
+  for (final file in files) {
+    if (file.path.contains('app_en.arb')) {
+      continue;
+    }
+
+    await file.delete();
+  }
+
+  print('current translations cleared');
+}
